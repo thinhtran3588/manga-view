23d9ea45b963dec01253eeeb5643d35649b18649
Thinh Tran <quangthinhtran3588@gmail.com>
2021-10-01 22:15:33 +0700

    fix build

--- __tests__/modules/main/screens/home/home.test.tsx
+++ __tests__/modules/main/screens/home/home.test.tsx
@@ -13,14 +13,35 @@ describe('Home', () => {
     expect(container).toMatchInlineSnapshot(`
 <div>
   <div
-    class="m-2"
+    class="flex flex-col w-full"
   >
-    demo
-    <button
-      class="bg-gradient dark:bg-gradient-light py-2 px-4 rounded-full text-white font-bold"
+    <div
+      class="flex rounded-full bg-white dark:bg-gray-600 p-2 shadow-xl w-full mb-2"
     >
-      View manga
-    </button>
+      <svg
+        class="h-6 w-6"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+        />
+      </svg>
+      <input
+        class="bg-transparent outline-none ml-2"
+        placeholder="Search manga"
+        value=""
+      />
+    </div>
+    <div
+      class="flex flex-row flex-wrap"
+    />
+    
   </div>
 </div>
 `);
