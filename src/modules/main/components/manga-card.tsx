9dab83c0078ca61c1b78627e02e09d2958376cc5
Thinh Tran <quangthinhtran3588@gmail.com>
2021-11-07 12:24:14 +0700

    disable prefetch for manga

--- src/modules/main/components/manga-card.tsx
+++ src/modules/main/components/manga-card.tsx
@@ -90,7 +90,7 @@ export const MangaCard = (props: MangaCardProps): JSX.Element => {
       )}
       CustomHeader={
         <div className='font-semibold pb-2 flex justify-center items-center'>
-          <Link href={`/manga/${manga.sourceId || '1'}/${manga.id}`}>
+          <Link href={`/manga/${manga.sourceId || '1'}/${manga.id}`} prefetch={false}>
             <a
               className={clsx(
                 'block flex-1 text-primary dark:text-primary-light',
